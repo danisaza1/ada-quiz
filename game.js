515a0856504fdc997627949d99b3e6085f00f789
danisaza1 <dani_isaza@htomail.com>
2025-02-17 15:21:28 +0100

    troisieme commit

---
+++ game.js
@@ -0,0 +1,5 @@
+import { quiz_culture_g } from './questions.js';
+
+
+
+
--- questions.js
+++ questions.js
@@ -1,77 +1,121 @@
 export const quiz_culture_g = {
 questions: [
     {
-    text: "Quel est le rôle des sourcils ? ",
+    text: "Quel est le rôle des sourcils ?",
     options: [
         " A - Les faire froncer quand on est en colère",
         " B - Empêcher la transpiration de tomber dans les yeux",
         " C - Décorer le visage",
         " D - Protéger les yeux des rayons UV",
     ],
-    correct_answer: " B - Empêcher la transpiration de tomber dans les yeux.",
-
+    correct_answer: " B - Empêcher la transpiration de tomber dans les yeux",
+    explication_answer: "Les sourcils ont la fonction de diriger la sueur et l'humidité loin des yeux !"
     },
 
     {
     text: "Quel animal peut frapper si fort qu'il fait bouillir l'eau autour de lui ?",
-
     options: [
         " A - Le poisson-globe ",
         " B - La crevette-mante",
         " C - Le calamard géant",
         " D - Le poisson-lune",
-
     ],
-    
     correct_answer: " B - La crevette-mante",
-
+    explication_answer: "Elle frappe avec une telle vitesse que cela crée une bulle de cavitation, ce qui chauffe l'eau à des températures extrêmes."  
     },
 
     {
-        text: 'Quel joueur est surnommé "CR7" ?',
-        options: [
-            " A - Lionel Messi",
-            " B - Carlos Rodriguez",
-            " C - Cristiano Ronaldo",
-            " D - Kylian Mbappé",
-        ],
-        correct_answer: "C - Cristiano Ronaldo ",
-    
-        },
+    text: 'Quel joueur est surnommé "CR7"?',
+    options: [
+        " A - Lionel Messi",
+        " B - Carlos Rodriguez",
+        " C - Cristiano Ronaldo",
+        " D - Kylian Mbappé",
+    ],
+    correct_answer: " C - Cristiano Ronaldo",
+    explication_answer: "CR7 est une légende du football, connu pour son numéro 7 et ses exploits avec le Real Madrid et le Portugal."
+    },
 
-        {
-            text: "Quel est le pays qui consomme le plus de chocolat ?",
-            options: [
-                " A - La Suisse",
-                " B - Les Etats-Unis",
-                " C - Le Japon ",
-                " D - Le Canada ",
-            ],
-            correct_answer: " B - La Suisse",
+    {
+    text: "Quel est le pays qui consomme le plus de chocolat ?",
+    options: [
+        " A - La Suisse",
+        " B - Les États-Unis",
+        " C - Le Japon ",
+        " D - Le Canada ",
+    ],
+    correct_answer: " A - La Suisse",
+    explication_answer: "La Suisse est réputée pour sa consommation massive de chocolat, et ses habitants sont parmi les plus grands amateurs de chocolat au monde !"    
+    },
+
+    {
+    text: "Quel animal peut dormir jusqu'à 3 ans si les conditions ne sont pas idéales ?",
+    options: [
+        " A - La grenouille",
+        " B - L'escargot",
+        " C - L'ours ",
+        " D - Le serpent ",
+    ],
+    correct_answer: " B - L'escargot",
+    explication_answer: "Les escargots peuvent entrer en hibernation ou en estivation pendant plusieurs années en fonction des conditions environnementales."    
+    },
+
+    {
+    text: "Qui est la joueuse la plus titrée au Ballon d'Or Féminin ?",
+    options: [
+        " A - Alexia Putellas",
+        " B - Marta",
+        " C - Megan Rapinoe",
+        " D - Ada Hegerberg",
+    ],
+    correct_answer: " A - Alexia Putellas",
+    explication_answer: "Alexia Putellas a remporté deux Ballons d'Or consécutifs en 2021 et 2022 avec le FC Barcelone."
+    },
+
+    {
+    text: 'Quel aliment peut "exploser" si tu le mets dans le micro-ondes sans le percer ?',
+    options: [
+        " A - Un œuf",
+        " B - Du chocolat",
+        " C - Une tomate",
+        " D - Une pomme de terre",
+    ],
+    correct_answer: " A - Un œuf",
+    explication_answer: "Si tu mets un œuf entier dans le micro-ondes sans le percer, la vapeur s’accumule et l'œuf peut éclater, créant un joli désastre."
+    },
+
+    {
+    text: "Quel animal dort en se tenant la main avec ses congénères ?",
+    options: [
+        " A - Le dauphin",
+        " B - L’otarie",
+        " C - La loutre de mer",
+        " D - Le castor",
+    ],
+    correct_answer: " C - La loutre de mer",
+    explication_answer: "Les loutres de mer se tiennent la main pendant leur sommeil pour ne pas dériver avec le courant."
+    },   
+
+    {
+    text: "Quelle nation a remporté la Coupe du Monde 2018 ?",
+    options: [
+        " A - Brésil",
+        " B - France",
+        " C - Argentine",
+        " D - Portugal",
+    ],
+    correct_answer: " B - France",
+    explication_answer: "La France a gagné la Coupe du Monde 2018 en battant la Croatie 4-2 en finale."
+    }, 
             
-            },
-
-            {
-                text: "Quel animal peut dormir jusqu'à 3 ans si les conditions ne sont pas idéales ? ",
-                options: [
-                    " A - La grenouille",
-                    " B - L'escargort",
-                    " C - L'ours ",
-                    " D - Le serpent ",
-                ],
-                correct_answer: " B - L'escargot",
-            
-                },
-            
-        
-
-
-
-]
-
-
-
-
-
-
-}
+    {
+    text: `À quelle heure peut-on commencer l'apéro ?`,
+    options: [
+        " A - A 18H", 
+        " B - A 19H",
+        " C - A 10H",
+        " D - Quand on veut !",
+    ],
+    correct_answer: " D - Quand on veut !",
+    },       
+]}
