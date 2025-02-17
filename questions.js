a07f9b5b0bbae4556458261a057e4885c507fd1f
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-02-17 13:26:24 +0100

    commit test push

--- questions.js
+++ questions.js
@@ -1,9 +1,69 @@
-export const quizz = {
+export const quiz_culture_g = {
 questions: [
-    text: "  ";
+    {
+    text: "Quel est le rôle des sourcils ? ",
     options: [
+        " A - Les faire froncer quand on est en colère",
+        " B - Empêcher la transpiration de tomber dans les yeux",
+        " C - Décorer le visage",
+        " D - Protéger les yeux des rayons UV",
+    ],
+    correct_answer: " B - Empêcher la transpiration de tomber dans les yeux.",
 
-    ]
+    },
+
+    {
+    text: "Quel animal peut frapper si fort qu'il fait bouillir l'eau autour de lui ?",
+
+    options: [
+        " A - Le poisson-globe ",
+        " B - La crevette-mante",
+        " C - Le calamard géant",
+        " D - Le poisson-lune",
+
+    ],
+    
+    correct_answer: " B - La crevette-mante",
+
+    },
+
+    {
+        text: 'Quel joueur est surnommé "CR7" ?',
+        options: [
+            " A - Lionel Messi",
+            " B - Carlos Rodriguez",
+            " C - Cristiano Ronaldo",
+            " D - Kylian Mbappé",
+        ],
+        correct_answer: "C - Cristiano Ronaldo ",
+    
+        },
+
+        {
+            text: "Quel est le pays qui consomme le plus de chocolat ?",
+            options: [
+                " A - La Suisse",
+                " B - Les Etats-Unis",
+                " C - Le Japon ",
+                " D - Le Canada ",
+            ],
+            correct_answer: " B - La Suisse",
+            
+            },
+
+            {
+                text: "Quel animal peut dormir jusqu'à 3 ans si les conditions ne sont pas idéales ? ",
+                options: [
+                    " A - La grenouille",
+                    " B - L'escargort",
+                    " C - L'ours ",
+                    " D - Le serpent ",
+                ],
+                correct_answer: " B - L'escargot",
+            
+                },
+            
+        
 
 
 
