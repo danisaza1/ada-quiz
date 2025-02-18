48023d4283f725c9e14d62feb1894c21bf75ab4b
GGui26 <gui.alexandre.fernandes.ca@gmail.com>
2025-02-18 15:15:57 +0100

    apres yoga

--- game.js
+++ game.js
@@ -1,23 +1,46 @@
+//Import de question
 import { quiz_culture_g } from './questions.js';
 
+//Récuperer les emplacements en HTML pour inserer les questions et les options 
 const questionsText = document.getElementById('question-text');
 const answersContainer = document.getElementById('options-container');
 const buttonNext = document.getElementById('next-button');
 
+//Variables pour suivre l'état du quiz
+let questionIndex =0;
 
-let currentQuestionIndex = quiz_culture_g.questions[0];
-questionsText.innerText = quiz_culture_g;
 
-currentQuestionIndex.options.forEach(options => {
+//Fonction pour afficher une question basée sur l'index actuel
+  function loadQuestion(){
+     // Vider le conteneur des options
+    answersContainer.innerHTML='';
+    // Récupérer la question actuelle
+    const currentQuestion=quiz_culture_g.questions[questionIndex];
+    // Injecter la question dans le HTML
+    questionsText.innerText = currentQuestion.questions;
+  //Pour chaque option on crée un boutton et on l'ajoute
+questionIndex.options.forEach(options => {
     const buttonAnswer = document.createElement('button');
     buttonAnswer.innerText = options;
     buttonAnswer.classList.add('options');
     answersContainer.appendChild(buttonAnswer);
   });
+  }
 
-// Fonction pour afficher une question basée sur l'index actuel
- // function loadQuestion(){
-     // Vider le conteneur des options
- //   answersContainer.innerHTML='';
-    //const currentQuestion
- // }
+
+  // Ajouter un écouteur d'événements pour le bouton "Suivant"
+buttonNext.addEventListener('click', () => {
+    // Incrémenter l'index de la question
+    questionIndex++;
+  
+    // Vérifier s'il reste des questions
+    if ( < __________.questions.length) {
+      // Afficher la question suivante
+      __________();
+    } else {
+      // Si plus de questions, indiquer la fin du quiz
+      __________.innerText = '__________';
+      __________.innerHTML = ''; // Effacer les options
+      __________.style.display = '__________'; // Cacher le bouton Suivant
+    }
+  });
