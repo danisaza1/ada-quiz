61073da6ea3854ea6caaa251137594ff75ae9a7a
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-02-20 10:33:19 +0100

    Etape 5 et 6

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ import { quiz_culture_g } from './questions.js';
 const questionsText = document.getElementById('question-text');
 const answersContainer = document.getElementById('options-container');
 const buttonNext = document.getElementById('next-button');
+const replayButton = document.getElementById('replay-button');
 
 //Variables pour suivre l'état du quiz
 let questionIndex =0;
@@ -15,32 +16,42 @@ let questionIndex =0;
      // Vider le conteneur des options
     answersContainer.innerHTML='';
     // Récupérer la question actuelle
-    const currentQuestion=quiz_culture_g.questions[questionIndex];
+    const currentQuestion =quiz_culture_g.questions[questionIndex];
     // Injecter la question dans le HTML
     questionsText.innerText = currentQuestion.questions;
   //Pour chaque option on crée un boutton et on l'ajoute
-questionIndex.options.forEach(options => {
+  currentQuestion.options.forEach(options => {
     const buttonAnswer = document.createElement('button');
     buttonAnswer.innerText = options;
     buttonAnswer.classList.add('options');
     answersContainer.appendChild(buttonAnswer);
   });
-  }
+}
 
 
   // Ajouter un écouteur d'événements pour le bouton "Suivant"
 buttonNext.addEventListener('click', () => {
-    // Incrémenter l'index de la question
-    questionIndex++;
-  
-    // Vérifier s'il reste des questions
-    if ( < __________.questions.length) {
-      // Afficher la question suivante
-      __________();
-    } else {
-      // Si plus de questions, indiquer la fin du quiz
-      __________.innerText = '__________';
-      __________.innerHTML = ''; // Effacer les options
-      __________.style.display = '__________'; // Cacher le bouton Suivant
-    }
-  });
+  // Incrémenter l'index de la question
+  questionIndex++;
+
+  // Vérifier s'il reste des questions
+  if ( questionIndex< quiz_culture_g.questions.length) {
+    // Afficher la question suivante
+    loadQuestion();
+  } else {
+    // Si plus de questions, indiquer la fin du quiz
+    answersContainer.innerText = ''; // Effacer les options
+    questionsText.innerHTML = ''; // Effacer les questions
+    buttonNext.style.display = 'none'; // Cacher le bouton Suivant
+    replayButton.style.display = 'inline-block';
+  }
+});
+
+loadQuestion();
+
+replayButton.addEventListener('click', () => {
+  questionIndex = 0;
+  replayButton.style.display = 'none';
+  buttonNext.style.display = 'inline-block';
+  loadQuestion();
+})
